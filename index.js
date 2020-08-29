cb01d35144f088b1c08a3a6c069a332fb738ea2c
Muhammad Elsayeh <mu.elsayeh@gmail.com>
2020-08-29 02:10:41 +0200

    Edited for better Maintainability

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import bodyParser from 'body-parser';
 import cosmosjs from '@cosmostation/cosmosjs'
 import { reset } from 'nodemon';
-import { Console } from 'console';
+//import { Console } from 'console';
 
 const chainId = process.env.CHAIN_ID;
 const lcdAddress = process.env.LCD_ADDRESS;
@@ -13,8 +13,8 @@ const amount = process.env.AMOUNT;
 const denom = process.env.DENOM;
 const minutes = parseInt(process.env.MINUTES);
 const memo = process.env.MEMO;
-const lcdhost = process.env.LCD_HOST;
-const lcdport = process.env.LCD_PORT;
+const amountLimit = parseInt(process.env.AMOUNT_LIMIT);
+const votingPowerLimit = parseInt(process.env.VOTING_POWER_LIMIT)
 const cosmos = cosmosjs.network(lcdAddress, chainId);
 
 cosmos.setPath(walletPath);
@@ -34,6 +34,8 @@ const app = express()
 const port = 3456
 
 const airdropInterval = minutes*60*1000;
+
+
 
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
@@ -55,7 +57,91 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//fuction check if the address has a validator with voting power higher than defined retriction rule 
+const gValidator = async(desmosAddress) => {
+    let url1 = lcdAddress+"/validatorsets/latest";
+    let url2= lcdAddress+'/staking/delegators/'+desmosAddress+'/validators';
+    let flagmV = false;
+   await  axios.all([
+        axios.get(url1),
+        axios.get(url2), 
+      // axios.get(url3),
+    ]).then(axios.spread((response1, response2) => {
+        let validatorsList = response1.data.result.validators;
+        let assocValidators = response2.data.result; 
+                        console.log("checking associated validator to the delegator address....")
+                        //console.log(assocValidators)      
+                        let assocValidatorsAddress = assocValidators.map(assocValidator => { return assocValidator.operator_address; });
+                        //console.log("Cross-linked Validators:....")
+                        //console.log(assocValidatorsAddress)
+                        let matchedValidators = validatorsList.filter(lv => assocValidatorsAddress.includes(lv.address));
+                        //console.log("Matched ones....")
+                        //console.log(matchedValidators)
+                        if(Array.isArray(matchedValidators) && matchedValidators.length){
+                        
+                            matchedValidators.forEach( (mValidator,i) => {
+                                matchedValidators[i].voting_power = parseInt(matchedValidators[i].voting_power);
+                               // console.log("stacking power: "+ matchedValidators[i].voting_power);
+                                if(matchedValidators[i].voting_power > votingPowerLimit){ 
+                                    flagmV = true; 
+                                }
+                            }); 
+                        }
+                        else{
+                            console.log("the address not associated to validator or the validtor is jailed")
+                            flagmV = false;
+                        }
+    }));
+    console.log("is it greedy validator :  " +flagmV)
+  return flagmV;
+}
 
+//function to check address's balance satisfy rules limits 
+
+const gAddress = async (desmosAddress) => {
+    let url3 = lcdAddress + '/bank/balances/'+desmosAddress;
+    let res = await axios.get(url3);
+    let flagmA = false;
+    let  balances = res.data.result;
+                        
+                        console.log("checking the balances with ....")
+                       // console.log(balances);
+    
+                        balances.forEach((balance, i) => {
+                        if (balances[i] && balances[i].amount)
+                            balances[i].amount = parseFloat(balances[i].amount);
+                         //   console.log("the amount: "+ balances[i].amount)
+                            if(balances[i].amount > amountLimit){
+                                flagmA = true; 
+                            }    
+                        }); 
+                        console.log("is it greedy address :  "+ flagmA)
+    return flagmA
+
+
+
+}
+
+// function that handle rules and restruction and check the validaity of the request for given desmos address 
+
+const rulesChecked  = async (ip, desmosAddress) => {
+    ip = ip.split(",").map(item => item.trim());
+    ip = ip[0];
+    let existingIP = historyStore
+        .find({ ip: ip })
+        .value()
+    let isGValidator  = await gValidator(desmosAddress);
+    let isGAddress = await gAddress(desmosAddress);
+
+    if (((typeof existingIP == "undefined") || (Date.now()-existingIP.airdropTime >= airdropInterval)) && (isGValidator ==  false)  && (isGAddress == false)){
+        console.log("Address is permitted for faucet.....");
+        return true;
+    }
+    else{
+        console.log("Address is rejected for fund.......");
+        return false;
+    }
+}
 
 
 app.get('/', function (req, res) {
@@ -68,62 +154,7 @@ app.get('/', function (req, res) {
 app.post('/airdrop', (req, res) => {
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     
-    ip = ip.split(",").map(item => item.trim());
-    ip = ip[0];
-    let existingIP = historyStore
-        .find({ ip: ip })
-        .value()
-    let flag_mV = false;
-    let flag_mA = false;
-    let desmos_address = req.body.address;
-    let url1 = lcdAddress+"/validatorsets/latest"
-    let url2= lcdAddress+'/staking/delegators/'+desmos_address+'/validators'
-    let url3 = lcdAddress + '/bank/balances/'+desmos_address;
-    axios.all([
-        axios.get(url1),
-        axios.get(url2), 
-        axios.get(url3),
-    ]).then(axios.spread((response1, response2, response3) => {
-        let validators_list = response1.data.result.validators;
-        let assoc_validators = response2.data.result; 
-                        console.log("checking associated validator to the delegator address....")
-                        console.log(assoc_validators)      
-                        let assoc_validators_address = assoc_validators.map(assoc_validator => { return assoc_validator.operator_address; });
-                        console.log("Cross-linked Validators:....")
-                        console.log(assoc_validators_address)
-                        let matched_validators = validators_list.filter(lv => assoc_validators_address.includes(lv.address));
-                        console.log("Matched ones....")
-                        console.log(matched_validators)
-                        if(Array.isArray(matched_validators) && matched_validators.length){
-                        
-                            matched_validators.forEach( (mValidator,i) => {
-                                matched_validators[i].voting_power = parseInt(matched_validators[i].voting_power);
-                                console.log("stacking power: "+ matched_validators[i].voting_power);
-                                if(matched_validators[i].voting_power > 200){ 
-                                        flag_mV = true; 
-                                }
-                            }); 
-                        }
-                        else{
-                            console.log("the address not associated to validator or the validtor is jailed")
-                        }
-    
-        let  balances = response3.data.result;
-                        
-                        console.log("checking the balances with ....")
-                        console.log(balances);
-    
-                        balances.forEach((balance, i) => {
-                        if (balances[i] && balances[i].amount)
-                            balances[i].amount = parseFloat(balances[i].amount);
-                            console.log("the amount: "+ balances[i].amount)
-                            if(balances[i].amount > 10000000){
-                                flag_mA = true; 
-                            }    
-                        }); 
-                        console.log("is it greedy validator :  " +flag_mV)
-                        console.log("is it greedy address :  "+ flag_mA)
-        if (((typeof existingIP == "undefined") || (Date.now()-existingIP.airdropTime >= airdropInterval)) && !flag_mA  && !flag_mV){
+    if (rulesChecked(ip,req.body.address)){
 
             cosmos.getAccounts(address).then(data => {
                 let stdSignMsg = cosmos.NewStdMsg({
@@ -162,9 +193,7 @@ app.post('/airdrop', (req, res) => {
         }
         //console.log(response1.data);
         //console.log(response2.data);
-    })).catch(error => {
-        console.log(error);
-    });
+   
     
      
 
